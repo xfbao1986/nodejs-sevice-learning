1a703bad3e6b3e1a9b9848851ce11ce6e5d67d92
xfbao1986 <xfbao1986@gmail.com>
2021-06-11 01:25:49 +0900

    update

--- exercises/ch-9/labs-1/app.js
+++ exercises/ch-9/labs-1/app.js
@@ -4,14 +4,21 @@ const app = express()
 const router = express.Router()
 const { PORT = 3000 } = process.env
 
-router.get('/', (req, res) => {
-  setTimeout(() => {
-    res.send((req.query.un || '').toUpperCase())
-  }, 1000)
+router.get('/', (req, res, next) => {
+    setTimeout(() => {
+        var un = req.query.un
+        if (typeof(un) === 'string') {
+            res.send((un).toUpperCase())
+        } else if (Array.isArray(un)) {
+            res.send(un.map(e => e.toUpperCase()))
+        } else {
+            next(new Error('Bad Request'))
+        }
+    }, 1000)
 })
 
 app.use(router)
 
 app.listen(PORT, () => {
-  console.log(`Express server listening on ${PORT}`)
+    console.log(`Express server listening on ${PORT}`)
 })
