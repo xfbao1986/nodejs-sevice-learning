6da8661161e781530dd43c4147e01933c587d627
xfbao1986 <xfbao1986@gmail.com>
2021-06-20 20:49:44 +0900

    update

--- final/routes/proxy/index.js
+++ final/routes/proxy/index.js
@@ -4,18 +4,19 @@ const proxy = require('fastify-http-proxy')
 const { Readable } = require('stream')
 
 async function* upper(res) {
-  for await (const chunk of res) {
-    yield chunk.toString().toUpperCase()
-  }
+    for await (const chunk of res) {
+        yield chunk.toString().toUpperCase()
+    }
 }
 
 module.exports = async (fastify, opts) => {
-  fastify.register(proxy, {
-    upstream: 'https://news.ycombinator.com/',
-    async preHandler(request, reply) {
-      if (request.query.token !== 'abc') {
-        throw fastify.httpErrors.unauthorized()
-      }
-    }
-  })
+    fastify.register(proxy, {
+        upstream: 'https://news.ycombinator.com/',
+        prefix: '/proxy',
+        async preHandler(request, reply) {
+            if (request.query.token !== 'abc') {
+                throw fastify.httpErrors.unauthorized()
+            }
+        }
+    })
 }
